95d742e4d9da61b93f97cd7027f7ecd3c5ed1692
MateoKappa <kapllanimateo01@gmail.com>
2023-04-22 03:07:36 +0300

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
-import Layout from "../components/Layout/Layout";
 import { NextUIProvider } from "@nextui-org/react";
+import Layout from "../Components/Layout/Layout";
 function MyApp({ Component, pageProps }) {
   return (
     <NextUIProvider>
